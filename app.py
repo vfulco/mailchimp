1858ed0583fd6a70a563c1bd323eaa72768ea8e7
Shashwat Singh <shashwatsingh@n3-43-207.dhcp.drexel.edu>
2018-11-30 19:43:25 -0500

    Added Add user to list feature and Updated YAML

--- app.py
+++ app.py
@@ -18,19 +18,41 @@ class Handler:
 		self.client = MailChimp(mc_api=os.getenv('API_KEY'), mc_user=os.getenv('USERNAME'))
 		return None
 
-	def printList(self):
+
+	def add_to_list(self):
 		req = request.get_json()
-		return self.jsonify(self.client.lists.all(get_all=True, fields="lists.name,lists.id"))
+		list_name = req['list_name']
+		user_email = req['user_email']
+		status = req['status']
+		first_name = req['first_name']
+		last_name = req['last_name']
+		list_id = self.get_listid(list_name)
 
+		try:
+			self.client.lists.members.create(list_id, {
+            'email_address': user_email,
+            'status': status,
+            'merge_fields': {
+            'FNAME': first_name,
+            'LNAME': last_name,
+            }})
+		except Exception as e:
+			return str(e)
 
-	def jsonify(self, data):
-		res = make_response(json.dumps(data))
-		return res
+		return "Successfully added {0} to {1} list!".format(first_name + " " + last_name, list_name)
+		
 
+	#get list ID from list name
+	def get_listid(self, list_name):
+		for x in self.client.lists.all(get_all=True, fields="lists.name,lists.id")['lists']:
+			if(x['name'] == list_name):
+				return x['id']
 
 	
 
 if __name__ == '__main__':
 	handler = Handler()
-	handler.app.add_url_rule('/add', 'add', handler.printList, methods=['post'])
+	handler.app.add_url_rule('/add', 'add', handler.add_to_list, methods=['post'])
 	handler.app.run(host='0.0.0.0', port=8000)
+
+
